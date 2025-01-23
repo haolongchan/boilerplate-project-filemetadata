23b37acfe1d5124a9bb454d7240072803b8098f1
Haolong <chl123.chenhaolong@gmail.com>
2025-01-23 02:39:35 -0500

    finish

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var cors = require('cors');
+const multer  = require('multer'); // Import multer
 require('dotenv').config()
 
 var app = express();
+var upload = multer({ storage: multer.memoryStorage() }); // Configure multer
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +13,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => { // Define the file analyse endpoint
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+  const file = req.file;
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
